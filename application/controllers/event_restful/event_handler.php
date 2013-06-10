2f1b97bd408abf228e4f3d2994f1112978b4575e
ynsekiz <ynsekiz@gmail.com>
2013-06-10 22:37:01 +0300

    köşeli parantez çözüldü

--- application/controllers/event_restful/event_handler.php
+++ application/controllers/event_restful/event_handler.php
@@ -8,16 +8,20 @@ class event_handler extends REST_Controller
 
     function get_event_get()
     {
- $this->load->model('event_model');             //loads model class of 'event_model'
+ 		$this->load->model('event_model');             //loads model class of 'event_model'
         $data = $this->event_model->readRow();         //get event_model's readRow method.
-
+/*
 		$unwanted = array('[',']');
 		$wanted = array('','');
 
 		$data = str_replace($unwanted,$wanted,$data);
-
-
+*/
+		foreach ($data as $d)
+		{
+			$d; /* 2 boyutlu array şeklinde gelen datayı tek boyuta indirgeyip
+					 array in son elemanını okur, yani "id" si en büyük olan evet ı okur*/
+		}
         
-        $this->response($data, 200);
+        $this->response($d, 200);
     }
 }
